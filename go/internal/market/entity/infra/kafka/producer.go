c7b7a27dfee954af5acb7b43d5fb6cd04add7df9
Aristófanes Lira <aristofanes.lira@gmail.com>
2023-06-28 14:55:30 -0300

    first commit

---
+++ go/internal/market/entity/infra/kafka/producer.go
@@ -0,0 +1,20 @@
+package kafka
+
+import ckafka "github.com/confluentinc/confluent-kafka-go/kafka"
+
+type Producer struct {
+	ConfigMap *ckafka.ConfigMap
+}
+
+func NewKafkaProducer(configMap *ckafka.ConfigMap) *Producer {
+	return &Producer{
+		ConfigMap: configMap,
+	}
+}
+
+func (p *Producer) Publish(msg interface{}, key []byte, topic string) error {
+	producer, err := ckafka.NewProducer(p.ConfigMap)
+	if err != nil {
+		return err
+	}
+}
